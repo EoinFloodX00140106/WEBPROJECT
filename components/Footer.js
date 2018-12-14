075dc574b264473420905f766819288a745e48fc
X00140106 <32164711+X00140106@users.noreply.github.com>
2018-12-14 13:49:32 +0000

    first commit

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
             <p>Contact information</p>
             <p>Mobile: +1-202-55-1038</p>
             <p>Email: Sports4U@gmail.com</p>
-
+           <p><a href="https://newsapi.org"> Powered by News API </a></p>
 
         </footer>
         <style jsx>{`
