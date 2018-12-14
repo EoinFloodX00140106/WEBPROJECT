1ed3a6f91f2b28f053305eb9dc38f7ad19354c1b
X00140106 <32164711+X00140106@users.noreply.github.com>
2018-12-14 11:32:07 +0000

    WEBPROJECT

---
+++ components/Footer.js
@@ -0,0 +1,24 @@
+// Footer component
+const Footer = () => (
+    <div>
+        <footer>
+            <p>Contact information</p>
+            <p>Mobile: +1-202-55-1038</p>
+            <p>Email: Sports4U@gmail.com</p>
+
+
+        </footer>
+        <style jsx>{`
+            footer {
+                background-color: #dcdcc6;
+                max-width: 1000px;
+                font-size: 1.8.0em;
+                text-align: center;
+                color: #101010;
+                clear: both;
+            }
+        `}</style>
+    </div> 
+ )
+ 
+ export default Footer;
